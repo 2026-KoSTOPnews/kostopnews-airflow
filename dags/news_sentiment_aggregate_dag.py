6745a6329d99e2f211de617352d2f2899add1e4f
Boyeong-G <goby0122@naver.com>
2026-09-24 00:10:35 +0900

    feat: add sentiment reaggregation logic and tests

--- dags/news_sentiment_aggregate_dag.py
+++ dags/news_sentiment_aggregate_dag.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 import pendulum
 
+from task.news_sentiment_aggregate_pipeline_tasks import reaggregate_sentiment_periods
 from task.news_sentiment_aggregate_pipeline_tasks import aggregate_sentiment_periods
 from task.news_sentiment_aggregate_pipeline_tasks import store_sentiment_aggregate
 
@@ -18,7 +19,24 @@ with DAG(
 
     store_sentiment_aggregate_task = PythonOperator(
         task_id="store_sentiment_aggregate",
-        python_callable=store_sentiment_aggregate
+        python_callable=store_sentiment_aggregate,
+        params={
+            "source_task_id": "aggregate_sentiment_periods"
+        }
+    )
+
+    reaggregate_sentiment_periods_task = PythonOperator(
+        task_id="reaggregate_sentiment_periods",
+        python_callable=reaggregate_sentiment_periods
+    )
+
+    store_sentiment_reaggregate_task = PythonOperator(
+        task_id="store_sentiment_reaggregate",
+        python_callable=store_sentiment_aggregate,
+        params={
+            "source_task_id": "reaggregate_sentiment_periods"
+        }
     )
 
     aggregate_sentiment_periods_task >> store_sentiment_aggregate_task
+    reaggregate_sentiment_periods_task >> store_sentiment_reaggregate_task
